11cfee29dd4428d12dc1c42bd5430384cfee9b1d
leegyu1222 <leegyu1222@gmail.com>
2015-11-30 16:05:26 +0900

    1차시연통합

--- 00.baerone/docs/sql/fare_ddl.sql
+++
@@ -1,8 +0,0 @@
-create table FARE(
-     address_gu       varchar2(12) not null,
-     latitude            number(3) not null,
-     longitude         number(3) not null
-);
-
-alter table fare
-add constraint pk_fare_address_gu primary key(address_gu);
